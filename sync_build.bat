79d085d913f8ed5e94e4f0bd712c2d18c13f6ad8
idea4good <idea4good@outlook.com>
2021-03-24 23:57:41 +0800

    solid solution for Win date

--- sync_build.bat
+++ sync_build.bat
@@ -1,5 +1,5 @@
 echo off
-SETLOCAL
+setlocal
 
 set argC=0
 for %%x in (%*) do Set /A argC+=1
@@ -10,37 +10,24 @@ if NOT "1" == "%argC%" (
 
 set url="https://api.powerbi.com/beta/72f988bf-86f1-41af-91ab-2d7cd011db47/datasets/2ff1e8a8-2f6f-4d73-a75d-86829e3f4574/rows?key=8f5xLp1gP8%%2FzSee4vCUBcyjR65I9zZ6nb%%2B%%2F7bbzex%%2FSctLX3ntIlAR0sxWpDdguuYyDtLdHK%%2Fxbxj%%2FrSBkX7eQ%%3D%%3D"
 
-rem For "monday mm/dd/yyyy"
-for /f "tokens=2-4 delims=-/., "  %%a in ("%date%") do (
-        set MM=%%a
-        set DD=%%b
-        set YYYY=%%c
-    )
+::analyze date time
+for /f %%x in ('wmic path win32_utctime get /format:list ^| findstr "="') do (
+    set %%x
+)
 
-rem For yyyy/mm/dd monday
-set YY=%YYYY:~0,2%
-if not "%YY%" == "20" (
-    for /f "tokens=1-3 delims=-/., "  %%a in ("%date%") do (
-            set YYYY=%%a
-            set MM=%%b
-            set DD=%%c
-        )
-    )
+Set Second=0%Second%
+Set Second=%Second:~-2%
+Set Minute=0%Minute%
+Set Minute=%Minute:~-2%
+Set Hour=0%Hour%
+Set Hour=%Hour:~-2%
+Set Day=0%Day%
+Set Day=%Day:~-2%
+Set Month=0%Month%
+Set Month=%Month:~-2%
+set datetime=%Year%-%Month%-%Day%T%Hour%:%Minute%:%Second%.000+0000
 
-rem For "dd/mm/yyyy monday"
-set YY=%YYYY:~0,2%
-if not "%YY%" == "20" (
-    for /f "tokens=1-3 delims=-/., "  %%a in ("%date%") do (
-            set YYYY=%%c
-            set MM=%%b
-            set DD=%%a
-        )
-    )
-
-set datetime=%YYYY%-%MM%-%DD%T%time: =0%0+0800
-set devie_info=Win-%USERNAME%
-
-rem ----------------- for GEO info -----------------
+::----------------- for GEO info -----------------
 set tmpPath=%userprofile%\AppData\Local\Temp\
 if not exist "%tmpPath%ip_info.tmp" (
     curl.exe ipinfo.io > %tmpPath%ip_info.tmp
@@ -71,13 +58,15 @@ set /p country=<%tmpPath%ip_country.tmp
 set /p city=<%tmpPath%ip_city.tmp
 set /p org=<%tmpPath%ip_org.tmp
 set raw_data=[{^
-\"device_info\" :\"%devie_info%\",^
+\"device_info\" :\"Win-%USERNAME%\",^
 \"project_info\" :\"%1\",^
 \"time\" :\"%datetime%\",^
 \"weight\" : 1,^
 \"country\" :\"%country%\",^
 \"city\" :\"%city%\",^
-\"org\" :\"%org%\"^
+\"org\" :\"%org%\",^
+\"log\" :\"%datetime%\",^
+\"version\" :\"v2.0\"^
 }]
 
 curl.exe --include --request POST --header "Content-Type: application/json" --data-binary "%raw_data%" "%url%"
